8b4da4b7f3974a32f65c30fed4c6bfb5a412ab34
hcheng826 <lilioo826@gmail.com>
2022-01-18 18:12:14 +0800

    deploy multiple contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,23 @@
 // This is a script for deploying your contracts. You can adapt it to deploy
 // yours, or create new ones.
+
+let TokenA = {
+  contractName: "TokenA",
+  initSupply: 10000,
+};
+
+let TokenB = {
+  contractName: "TokenB",
+  initSupply: 10000,
+};
+
+let OrderBook = {
+  contractName: "OrderBook",
+}
+
+contracts = [TokenA, TokenB, OrderBook];
+// const secrets = require('../secrets.json');
+
 async function main() {
   // This is just a convenience check
   if (network.name === "hardhat") {
@@ -11,7 +29,11 @@ async function main() {
   }
 
   // ethers is avaialble in the global scope
+
+  // deploy with default first address of local hardhat test account
   const [deployer] = await ethers.getSigners();
+  // deploy with your own address
+  // const deployer = new ethers.Wallet(`0x${secrets.ACCOUNT_DEV_PRIVATE_KEY}`, new ethers.providers.JsonRpcProvider());
   console.log(
     "Deploying the contracts with the account:",
     await deployer.getAddress()
@@ -19,17 +41,26 @@ async function main() {
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-  await token.deployed();
+  TokenA.contract = await ethers.getContractFactory(TokenA.contractName, deployer);
+  TokenA.contract = await TokenA.contract.deploy(TokenA.initSupply);
+  await TokenA.contract.deployed();
+  console.log(`${TokenA.contractName} contract address:`, TokenA.contract.address);
 
-  console.log("Token address:", token.address);
+  TokenB.contract = await ethers.getContractFactory(TokenB.contractName, deployer);
+  TokenB.contract = await TokenB.contract.deploy(TokenB.initSupply);
+  await TokenB.contract.deployed();
+  console.log(`${TokenB.contractName} contract address:`, TokenB.contract.address);
+
+  OrderBook.contract = await ethers.getContractFactory(OrderBook.contractName, deployer);
+  OrderBook.contract = await OrderBook.contract.deploy(TokenA.contract.address, TokenB.contract.address);
+  await OrderBook.contract.deployed();
+  console.log(`${OrderBook.contractName} contract address:`, OrderBook.contract.address);
 
   // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(token);
+  saveFrontendFiles([TokenA, TokenB, OrderBook]);
 }
 
-function saveFrontendFiles(token) {
+function saveFrontendFiles(contracts) {
   const fs = require("fs");
   const contractsDir = __dirname + "/../frontend/src/contracts";
 
@@ -37,16 +68,21 @@ function saveFrontendFiles(token) {
     fs.mkdirSync(contractsDir);
   }
 
+  contractAddress = {};
+  contracts.forEach((contract) => {
+    const ContractArtifact = artifacts.readArtifactSync(`${contract.contractName}`);
+
+    fs.writeFileSync(
+      contractsDir + `/${contract.contractName}.json`,
+      JSON.stringify(ContractArtifact, null, 2)
+    );
+
+    contractAddress[contract.contractName] = contract.contract.address;
+  });
+
   fs.writeFileSync(
     contractsDir + "/contract-address.json",
-    JSON.stringify({ Token: token.address }, undefined, 2)
-  );
-
-  const TokenArtifact = artifacts.readArtifactSync("Token");
-
-  fs.writeFileSync(
-    contractsDir + "/Token.json",
-    JSON.stringify(TokenArtifact, null, 2)
+    JSON.stringify(contractAddress, undefined, 2)
   );
 }
 
